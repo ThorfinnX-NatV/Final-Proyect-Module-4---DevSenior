60f993e8a6741181a2a911996a0c78bbb5cfb0eb
TheRedEntity <red.romero.023@gmail.com>
2026-08-05 19:59:07 -0500

    Modelos - Bosquejo Venta y Producto

---
+++ models/producto.py
@@ -0,0 +1,21 @@
+class Producto:
+    def __init__(self):
+        '''Constructor de la Clase Producto
+
+            Este Metodo Recibira los atributos que son necesarios para la representacion de un Producto
+        '''
+
+    def mostrar(self):
+        '''Muestra la informacion de un Producto '''
+        print("Metodo al Pendiente de implementar")
+
+    def aumentar_stock(self):
+        '''Aumenta el Stock disponible de un Producto'''
+        print("Metodo de Aumentar el Stock")
+
+    def disminuir_stock(self):
+        '''Disminuye el Stock disponible de un Procucto'''
+        print("Metodo de Disminuir el Stock")
+
+    def stock_disponible (self):
+        '''Verifica si hay cantidad Suficiente de Stock'''
---
+++ models/venta.py
@@ -0,0 +1,12 @@
+class Venta:
+    def __init__(self):
+
+        '''Construictor de la clase Venta'''
+
+    def calcular_total (self):
+        '''Calcula el Total de una Venta '''
+        print("Calcular el total de una venta Pendiente")
+
+    def info_venta (self):
+        '''Muestra la informacion o los detalles de una venta'''
+        print("Metodo de mostrar informacion de venta Pendiente")
