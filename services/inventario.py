287c7ef41cfedf7c8e6745c0cd0b9cbfe937f642
TheRedEntity <red.romero.023@gmail.com>
2026-08-05 20:15:03 -0500

    Bosquejo de Servicios - Readme's Actualizados

---
+++ services/inventario.py
@@ -0,0 +1,27 @@
+class Inventario:
+
+
+    def __init__(self):
+        '''Se representa el inventario en una estructura con forma de lista, debido al orden de insercion '''
+
+        self.producto = []
+
+    def registrar_producto (self):
+        '''Funcion que permite agregar productos al inventario'''
+        print("\n [Inventario]")
+        print("Registro del Producto pendiente")
+
+    def listar_producto (self):
+        '''Funcion que Lista o muestra ordenadamente el inventario'''
+        print("\n [Inventario]")
+        print("Listar Inventario pendiente")
+
+    def buscar_producto (self):
+        '''Busca un producto en el inventario'''
+        print("\n [Inventario]")
+        print("Buscar en el Inventario Pendiente")
+
+    def eliminar_producto (self):
+        '''Elimina un producto del inventario'''
+        print("\n [Inventario]")
+        print("Eliminar producto Pendiente")
---
+++ services/ventas.py
@@ -0,0 +1,17 @@
+class Ventas:
+
+    def __init__(self):
+        '''Se almacenara en una lista para tener un registro y y orden de llegada en orden'''
+        self.lista = []
+
+    def registrar_ventas (self):
+        print("\n Ventas")
+        print("Registra ventas Pendientes")
+
+    def listar_ventas (self):
+        print("\n Ventas")
+        print("Lista de Ventas Pendientes")
+
+    def total_vendido (self):
+        print("\n Ventas")
+        print("Total vendido en ventas pendientes")
